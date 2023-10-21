645b0e1a63434718af656a432303f1961703f9da
Spolavore <75754941+Spolavore@users.noreply.github.com>
2023-10-21 12:54:24 -0300

    finalização parcial da parte de tecnologias

--- src/componets/Tecnologias/TecnologiaBox/index.jsx
+++ src/componets/Tecnologias/TecnologiaBox/index.jsx
@@ -1,10 +1,12 @@
 import style from './styles.module.css'
+
 export default function Box(props){
+    
     return(
         <div className={style.box}>
             <h2>{props.title}</h2>
             <p>{props.desc}</p>
-            <img src={props.img_src} style={{width: '100px'}}/>
+            <img src={props.img_src} style={{width: '100px'}} alt=''/>
             <button>Ver mais</button>
         </div>
     )
--- src/componets/Tecnologias/carrousel.jsx
+++ src/componets/Tecnologias/carrousel.jsx
@@ -23,28 +23,28 @@ export default function Carrousel(){
         <div className={style.container} >
             <OwlCarousel className='owl-theme' style={{width: '100%'}} {...options}  >
                 <div className={style.item}>
-                    <Box img_src={'React-icon.png'} title={'React Js'}/>
+                    <Box img_src={'atomo.png'} title={'React Js'} desc={"Conhecimento sobre components e boas práticas no código, utilização de hooks, React Routes, consumo de API's e informações de base de dados."}/>
                 </div>
                 <div className={style.item}>
-                    <Box  img_src={'arquivo-python.png'} title={'Python'} desc={'Conhecimento de estrutura de dados, utilização de bibliotecas, automatização de processos e desenvolvimento em ambientes virtuais'}/>
+                    <Box  img_src={'arquivo-python.png'} title={'Python'} desc={'Conhecimento de estrutura de dados, utilização de bibliotecas, automatização de processos e desenvolvimento em ambientes virtuais.'}/>
                 </div>
                 <div className={style.item}>
-                    <Box  img_src={'javascript.png'} title={'Javascript'} desc={'Conhecimento de utilização fundamental'}/>
+                    <Box  img_src={'javascript.png'} title={'Javascript'} desc={'Conhecimento de utilização fundamental, obtenção de dados do cliente, dinamização de páginas e sua aplicação em frameworks.'}/>
                 </div>
                 <div className={style.item} >
-                    <Box  img_src={'desenvolvimento-de-software.png'} title={'HTML5'}/>
+                    <Box  img_src={'html.png'} title={'HTML5'} desc={'Conhecimento amplo da linguagem e sua tags, além da sua utilização específica em frameworks.'}/>
                 </div>
                 <div className={style.item} >
-                    <Box  img_src={'designer-de-web (1).png'} title={'CSS'}/>
+                    <Box  img_src={'css (1).png'} title={'CSS'} desc={'Conhecimento avançado de css, utilização de flex, animações com css puro, responsividade e acessibilidade web.'}/>
                 </div>
                 <div className={style.item} >
-                    <Box  img_src={'logo-git.png'} title={'Git & Github'}/>
+                    <Box  img_src={'git.png'} title={'Git & Github'} desc={'Conhecimento de boas práticas no git, entendimento sobre: branches, pull requests, merges, resolução de conflitos no código, entre outras...'}/>
                 </div>
                 <div className={style.item} >
-                    <Box  img_src={'logo-C.png'} title={'C'}/>
+                    <Box  img_src={'c-.png'} title={'C & C++'} desc={'Conhecimento básico sobre a linguagem e tipação de dados.'}/>
                 </div>
                 <div className={style.item} >
-                    <Box  img_src={'big-data.png'} title={'Postgresql'}/>
+                    <Box  img_src={'big-data.png'} title={'Postgresql'} desc={'Conhecimento sobre funcionalidades do SGBD e técnicas para uma boa construção estrutural do banco utilizando a metodologia de modelagem ER'}/>
                 </div>big-data.png
          </OwlCarousel>
         </div>
