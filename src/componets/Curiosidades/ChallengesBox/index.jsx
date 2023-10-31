3822bd81968fcef81a299ba3b0a94018835f2e65
João Vitor Do Amaral Spolavore <j.vitor.spolavore@gmail.com>
2023-10-31 15:43:46 -0300

    arrumando desafaios
    falta ainda disponibiliar o link

--- src/componets/Curiosidades/ChallengesBox/index.jsx
+++ src/componets/Curiosidades/ChallengesBox/index.jsx
@@ -3,7 +3,13 @@ import styles from './styles.module.css'
 import ChallengeInfo from './ChallengeInfos';
 export default function ChallengeBox(props){
     var desafios_interessante = ['convert-pascalcase-string-into-snake-case', 'first-non-repeating-character', 'extract-the-domain-name-from-a-url-1']
+    var nomes_desafios = []
     const [dataChallenge,setDataChallenge] = useState([])
+    desafios_interessante.map((item,index) => {
+        nomes_desafios.push(item.split('-').join(' '))
+    })
+
+    console.log(nomes_desafios)
     useEffect(()=>{
         for(let i = 0; i < desafios_interessante.length; i += 1){
             fetch(`https://www.codewars.com/api/v1/code-challenges/${desafios_interessante[i]}`)
@@ -15,7 +21,7 @@ export default function ChallengeBox(props){
     return( 
         <div className={styles.container}>
             {desafios_interessante.map((item,index)=>{
-                return(<ChallengeInfo key={index*10} name={item} link={'https://www.codewars.com/users/Spola/completed_solutions'} description={dataChallenge[index]?.description}/>)
+                return(<ChallengeInfo key={index*10} name={nomes_desafios[index]} link={'https://www.codewars.com/users/Spola/completed_solutions'} description={dataChallenge[index]?.description}/>)
             })}
         </div>
     )
