4e145764b6ba3277b35a660ef56f06aafab40b50
Spolavore <75754941+Spolavore@users.noreply.github.com>
2023-11-08 22:25:25 -0300

    ajustes de responsividade

--- src/componets/Contato/index.jsx
+++ src/componets/Contato/index.jsx
@@ -1,6 +1,36 @@
+import { useState } from "react"
 import Title from "../Title"
 import styles from './styles.module.css'
+import emailjs from '@emailjs/browser';
 export default function Contato(){
+    const [nome, setNome] = useState('')
+    const [email, setEmail] = useState('')
+    const [assunto, setAssunto] = useState('')
+
+
+    const [styleInvalid, setStyleInvalid] = useState(null)
+
+    function sendEmail(e){
+        e.preventDefault()
+ 
+        if(nome === '' || email === '' || assunto === ''){
+            setStyleInvalid({border: '2px solid red'})
+            alert('Preencha todos os campos')
+            return;
+        }
+        const templateParams = {
+            from_name: nome,
+            message: assunto,
+            email: email
+        }
+        emailjs.send("service_4nba2dm", "template_wqc47fu", templateParams, "MeMdCYDeUs9coBz5I")
+        .then((response) => {
+            alert("Email Enviado")
+            setNome('')
+            setEmail('')
+            setAssunto('')
+        }, (err) => alert('Erro ao enviar email'))
+    }
     return(
         <>
         <div id="contato" style={{height: "100px", backgroundColor: 'white'}}></div>
@@ -9,14 +39,15 @@ export default function Contato(){
 
             <div className={styles.contato}>
                 <p>Gostou de algo e gostaria de me contactar? Mande uma mensagem para mim :&#41;</p>
+                <form onSubmit={sendEmail}>
                 <div className={styles.inputList}>
-                    <input placeholder="Nome"></input>
-                    <input placeholder="Email"></input>
-                    <input placeholder="Assunto"></input>
-                
-
+                    <input placeholder="Digite seu nome"  style={styleInvalid} value={nome} onChange={(e)=> setNome(e.target.value)}></input>
+                    <input type="email" placeholder="Digite seu email"  style={styleInvalid} value={email} onChange={(e)=> setEmail(e.target.value)}></input>
+                    <textarea placeholder="Assunto" style={styleInvalid} value={assunto} onChange={(e)=> setAssunto(e.target.value)}></textarea>
+    
                     <button>Enviar</button>
                 </div>
+                </form>
             </div>
         </div>
         </>
